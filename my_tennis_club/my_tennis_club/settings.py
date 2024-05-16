3e6141b2fd5c374654dcd303f6230c36e62f6496
Phylosius <hei.phylosius@gmail.com>
2024-05-16 15:30:20 +0300

    finished Admin courses

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+9!=6e8%mj+b!6un+hh(k2d$#biox%^@vktc-!(p_qu%m*^@9i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members'
 ]
 
 MIDDLEWARE = [
